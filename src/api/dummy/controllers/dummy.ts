a783fff962102f1fbccf49ee6d0432cb7d28cbe8
LemurSage <zainjee37405@gmail.com>
2024-09-29 21:44:09 +0500

    Add dummy API components and content type schema

---
+++ src/api/dummy/controllers/dummy.ts
@@ -0,0 +1,7 @@
+/**
+ * dummy controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dummy.dummy');
---
+++ src/api/dummy/services/dummy.ts
@@ -0,0 +1,7 @@
+/**
+ * dummy service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::dummy.dummy');
